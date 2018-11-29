b5b77b43793edc96e2591767ff9caa9e2debb6c6
Benjamin O'Connor <benoc.formal@gmail.com>
2018-11-28 23:05:24 -0800

    added fully functional light/dark colorscheme toggle to be stylized and given icons

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+const root = document.documentElement
+
 function openNav() {
     document.getElementById("socialMediaNav").style.width = "100px";
     document.getElementById("main").style.marginLeft = "0px";
@@ -9,6 +11,19 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
 }
 
+function toggleLightScheme() {
+	root.style.setProperty('--boxColor', '#EFE9F4');
+	root.style.setProperty('--textColor', '#171D1C');
+	root.style.setProperty('--shadeColor', '#87919E');
+	root.style.setProperty('--backgroundColor', '#FFFFFF');
+
+}
 
 
+function toggleDarkScheme() {
+	root.style.setProperty('--boxColor', '#373f51');
+	root.style.setProperty('--textColor', '#D8DBE2');
+	root.style.setProperty('--shadeColor', '#87919E');
+	root.style.setProperty('--backgroundColor', '#202030');
+}
 
