097a3f491f3847c05ec59c9d15642b4056f37949
Benjamin O'Connor <benoc.formal@gmail.com>
2018-11-29 22:27:27 -0800

    Moved color scheme options into side bar and made color scheme choice persistant until browser close

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 
 const root = document.documentElement
 
-function openNav() {
-    document.getElementById("socialMediaNav").style.width = "100px";
+
+function onLoad() {
+
+
+	var chosenScheme = document.cookie;
+
+	if (chosenScheme =='chosenColorScheme=Dark') {
+		toggleDarkScheme();
+	} 
+	if (chosenScheme =='chosenColorScheme=Light') {
+		toggleLightScheme();
+	}
+
+	console.log(document.cookie);
+	console.log(chosenScheme);
+}
+
+function openSocialMediaNav() {
+    document.getElementById("socialMediaNav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "0px";
 }
 
-function closeNav() {
+function closeSocialMediaNav() {
     document.getElementById("socialMediaNav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+}
+
+function openColorSchemePicker() {
+    document.getElementById("colorSchemePicker").style.width = "200px";
+    document.getElementById("main").style.marginRight = "0px";
+}
+
+function closeColorSchemePicker() {
+    document.getElementById("colorSchemePicker").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
 }
 
 function toggleLightScheme() {
@@ -16,14 +43,13 @@ function toggleLightScheme() {
 	root.style.setProperty('--textColor', '#171D1C');
 	root.style.setProperty('--shadeColor', '#87919E');
 	root.style.setProperty('--backgroundColor', '#FFFFFF');
-
+	document.cookie = "chosenColorScheme=Light; path=/";
 }
-
 
 function toggleDarkScheme() {
 	root.style.setProperty('--boxColor', '#373f51');
 	root.style.setProperty('--textColor', '#D8DBE2');
 	root.style.setProperty('--shadeColor', '#87919E');
 	root.style.setProperty('--backgroundColor', '#202030');
+	document.cookie = "chosenColorScheme=Dark; path=/";
 }
-
